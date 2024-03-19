b84703cb1f446d375ace5a6182b482d299bef4a4
TariqKichawele <tariqkichawele66@gmail.com>
2024-03-19 15:58:40 -0400

    add search functionality and component

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -25,11 +25,11 @@ const MoviePage = async ({ params }) => {
                 </h2>
                 <p className='text-lg mb-3'>{movie.overview}</p>
                 <p className='mb-3'>
-                    <span className='font-semibold mr-1'>Date Released:</span>
+                    <span className='font-semibold mr-1'>Date Released: </span>
                     {movie.release_date || movie.first_air_date}
                 </p>
                 <p className='mb-3'>
-                    <span className='font-semibold'>Rating:</span>
+                    <span className='font-semibold'>Rating: </span>
                     {movie.vote_count}
                 </p>
             </div>
